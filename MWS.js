05eb42ce2dc052b69f99369e4b1923c96563b10f
umbobabo <umbertobabini@economist.com>
2015-05-22 16:44:20 +0100

    Added helper fo HB

--- MWS.js
+++ MWS.js
@@ -7,112 +7,8 @@ var app = express(), hbs;
 
 var conf = {
   "app": {
-    port: 3100,
-    repoFolder: "mnv"
-  },
-  "templates": {
-    "folder": "dist/tpl"
+    port: 3100
   }
-};
-
-var data = {
-   "company-info":[
-      {
-         "link_title":"Advertise",
-         "link_path":"http:\/\/www.economistgroupmedia.com\/"
-      },
-      {
-         "link_title":"Reprints",
-         "link_path":"http:\/\/www.economist.com\/rights\/"
-      },
-      {
-         "link_title":"Careers",
-         "link_path":"http:\/\/www.economistgroupcareers.com\/"
-      },
-      {
-         "link_title":"Editorial staff",
-         "link_path":"http:\/\/www.economist.com\/mediadirectory"
-      },
-      {
-         "link_title":"Site Map",
-         "link_path":"http:\/\/www.economist.com\/content\/site-index"
-      },
-      {
-         "link_title":"The Economist Group",
-         "link_path":"http:\/\/www.economistgroup.com\/"
-      }
-   ],
-   "contact":[
-      {
-         "link_title":"Subscribe",
-         "link_path":"http:\/\/www.economist.com\/products\/subscribe"
-      },
-      {
-         "link_title":"Contact us",
-         "link_path":"http:\/\/www.economist.com\/contact-info"
-      },
-      {
-         "link_title":"Help",
-         "link_path":"http:\/\/www.economist.com\/help\/home"
-      }
-   ],
-   "legal":[
-      {
-         "link_title":"Accessibility",
-         "link_path":"http:\/\/www.economist.com\/help\/accessibilitypolicy"
-      },
-      {
-         "link_title":"Cookies",
-         "link_path":"http:\/\/www.economist.com\/cookies-info"
-      },
-      {
-         "link_title":"Privacy",
-         "link_path":"http:\/\/www.economistgroup.com\/results_and_governance\/governance\/privacy"
-      },
-      {
-         "link_title":"Terms of use",
-         "link_path":"http:\/\/www.economist.com\/legal\/terms-of-use"
-      }
-   ],
-   "social-header":[
-      {
-         "title":"Follow us"
-      }
-   ],
-   "social-channel":[
-      {
-         "social_icon":"icon-facebook3",
-         "social_link_path":"https:\/\/www.facebook.com\/TheEconomist\/"
-      },
-      {
-         "social_icon":"icon-twitter3",
-         "social_link_path":"https:\/\/twitter.com\/TheEconomist\/"
-      },
-      {
-         "social_icon":"icon-feed4",
-         "social_link_path":"http:\/\/www.economist.com\/rss\/"
-      },
-      {
-         "social_icon":"icon-mail",
-         "social_link_path":"https:\/\/twitter.com\/TheEconomist\/"
-      },
-      {
-         "social_icon":"icon-google-plus3",
-         "social_link_path":"https:\/\/plus.google.com\/+TheEconomist\/posts\/"
-      },
-      {
-         "social_icon":"icon-youtube3",
-         "social_link_path":"http:\/\/www.youtube.com\/user\/economistmagazine\/"
-      },
-      {
-         "social_icon":"icon-linkedin",
-         "social_link_path":"http:\/\/www.linkedin.com\/groups\/Economist-official-group-Economist-newspaper-3056216\/"
-      },
-      {
-         "social_icon":"icon-pinterest",
-         "social_link_path":"http:\/\/www.pinterest.com\/theeconomist\/"
-      }
-   ]
 };
 
 var availableViews = fs.readdirSync('views');
@@ -127,15 +23,26 @@ app.set('view engine', 'handlebars');
 
 app.get('/TheWorldIf/:article?', function (req, res) {
   var article = req.params.article || null;
+  // Mockup data
   var storytilesData = JSON.parse(fs.readFileSync('data/theWorldIf/storytiles.json', 'utf8'));
   var footerData = JSON.parse(fs.readFileSync('data/footer.json', 'utf8'));
+  // End of Mockup data
   hbs.partialsDir = ['bower_components'];
   res.render('theWorldIfBody', {
       layout: 'theWorldIf',
       article: article,
       // TODO Review this part, sucks!
       tile: storytilesData['ec-storytilesreveal-0'].tile,
-      "mnv-cmp-footer": footerData['mnv-cmp-footer']
+      "mnv-cmp-footer": footerData['mnv-cmp-footer'],
+      helpers: {
+        ifvalue: function(conditional, options) {
+          if (conditional == options.hash.equals) {
+            return options.fn(this);
+          } else {
+            return options.inverse(this);
+          }
+        }
+      }
     });
 });
 
