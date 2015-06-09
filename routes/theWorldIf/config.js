74485896bee6cbe344f73a2a497f9077a95a4ae0
umbobabo <umbertobabini@economist.com>
2015-06-09 09:51:29 +0100

    Added Drupal test configuration

--- routes/theWorldIf/config.js
+++ routes/theWorldIf/config.js
@@ -1,3 +1,13 @@
+// Drupal local ENV
+// exports.config = {
+//   homeURL: 'TheWorldIf',
+//   feed: {
+//     host: '127.0.0.1:8083',
+//     landingPath: 'json/worldin/landing/page',
+//     articlePath: 'json/worldin/article'
+//   }
+// };
+
 exports.config = {
   homeURL: 'TheWorldIf',
   feed: {
